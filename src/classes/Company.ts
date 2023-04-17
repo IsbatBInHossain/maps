9e84486a3cee896d96da012a0e419000995ade61
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-04-17 16:24:05 +0600

    Add User and Company classes

--- src/classes/Company.ts
+++ src/classes/Company.ts
@@ -1,13 +1,15 @@
 import { faker } from '@faker-js/faker';
 
-export class User {
-  name: string;
+export class Company {
+  companyName: string;
+  catchphrase: string;
   location: {
     lat: number;
     lng: number;
   };
   constructor() {
-    this.name = `${faker.name.firstName()} ${faker.name.lastName()}`;
+    this.companyName = faker.company.name();
+    this.catchphrase = faker.company.catchPhrase();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import User from './classes/User';
-
-const greeting: string = 'Hello, Vite!';
+import { Company } from './classes/Company';
+import { User } from './classes/User';
+const user = new User();
+const company = new Company();
 
 document.getElementById('app')!.innerHTML = `
-  <h1>${greeting}</h1>
+  <h1>${user.name}</h1>
+  <p>laitude: ${user.location.lat}, longitude: ${user.location.lng}</p>
+  <h1>${company.companyName}</h1>
+  <h3>${company.catchphrase}</h3>
+  <p>laitude: ${company.location.lat}, longitude: ${company.location.lng}</p>
 `;
